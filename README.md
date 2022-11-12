e8840c309a28d69604717f15a247c9c6d8d379ca
Nícolas Hess <nicolashess1@gmail.com>
2022-11-12 01:49:39 -0300

    cleaning data and structuring the dataset better

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ download_imdb_tables
 download_name_data
 concat_info_movie
 get_ratings
+clean_data
--- scripts/get_ratings.py
+++ scripts/get_ratings.py
@@ -9,6 +9,6 @@ df["release_year"] = df["release_year"].map(str)
 ratings['name_year'] = ratings['Name'] + ' ' + ratings['Year']
 df["name_year"] = df["title"] + ' '+ df["release_year"]
 
-total = df.merge(ratings, on='name_year', how='left').drop(["Name", "name_year", "Date", "Year", "Letterboxd URI"], axis=1)
-total.to_csv("../data/clean/ratings_watched.csv")
+total = df.merge(ratings, on='name_year', how='left')
+total.to_csv("../data/clean/dirty_watched.csv")
 print("Done")
