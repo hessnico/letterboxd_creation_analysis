6a1bcb54c2e05cd548242ed23a17682e96036112
Nícolas Hess <nicolashess1@gmail.com>
2022-11-15 15:24:52 -0300

    readme.md file updated

--- scripts/index.py
+++ scripts/index.py
@@ -1,18 +1,20 @@
 import os 
+import subprocess
 
 default = os.getcwd()
 
-exec(open("./get_movies_data.py").read())
+folders = ["clean", "created", "imdb", "letterboxd"]
+for folder in folders:
+    target_dir = f"../data/{folder}"
+    if os.path.exists(target_dir) == False:
+        os.mkdir(target_dir)
+
+files = ["./get_movies_data.py", "./concat_watched_watchlist.py", "./download_imdb_tables.py", "./download_imdb_tables.py", "./download_name_data.py", "./concat_info_movie.py", "./get_ratings.py", "./clean_data.py"]
+files = []
+
+for file in files:
+    os.chdir(default)
+    exec(open(file).read())
+
 os.chdir(default)
-exec(open("./concat_watched_watchlist.py").read())
-os.chdir(default)
-exec(open("./download_imdb_tables.py").read())
-os.chdir(default)
-exec(open("./download_name_data.py").read())
-os.chdir(default)
-exec(open("./concat_info_movie.py").read())
-os.chdir(default)
-exec(open("./get_ratings.py").read())
-os.chdir(default)
-exec(open("./clean_data.py").read())
-os.chdir(default)
+subprocess.call("create_pdf_notbook.bat")
